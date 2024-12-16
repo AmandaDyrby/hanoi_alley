5f705dd3d9a520cbbd8c4ef25251bc3a83130669
Emma Munch <emmasofie.munch@gmail.com>
2024-12-16 15:30:10 +0100

    book bord

--- javascript.js
+++ javascript.js
@@ -1,3 +1,20 @@
+// HVOR MANGE GÆSTER
+
+function changeGuests(value) {
+  const input = document.getElementById("guests");
+  let currentValue = parseInt(input.value);
+  if (currentValue + value >= 1) {
+    input.value = currentValue + value;
+  }
+}
+
+// NY KNAP NÅR MAN TRYKKER FORSÆT
+
+function showContactFields() {
+  const contactFields = document.getElementById("contactFields");
+  contactFields.style.display = "block";
+}
+
 document.getElementById("menuButton").addEventListener("click", () => {
   const menu = document.getElementById("dropdownMenu");
   if (menu.style.display === "block") {
@@ -6,3 +23,5 @@ document.getElementById("menuButton").addEventListener("click", () => {
     menu.style.display = "block";
   }
 });
+
+// MENUEN
